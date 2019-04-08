9fc7b026a101061c8dfde8248c44bd89428f6c5c
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-08 13:22:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,10 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
+<<<<<<< HEAD
 def valid_move?(board, index)
+=======
+>>>>>>> 2064ffbb7e534761cec86c46f1895c3e6c2cdee9
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
       return false
@@ -11,6 +14,7 @@ def valid_move?(board, index)
       return true
     end
   end
+<<<<<<< HEAD
   
   def on_board?(num)
     if num.between?(0, 8) == true
@@ -26,3 +30,5 @@ if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return false
   end
 end
+=======
+>>>>>>> 2064ffbb7e534761cec86c46f1895c3e6c2cdee9
